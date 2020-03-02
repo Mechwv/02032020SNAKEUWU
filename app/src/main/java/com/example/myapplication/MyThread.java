8a69a241ce9f5d369f10e8170367d7e580903db6
Profile <test@gmail.com>
2020-03-02 10:20:33 +0300

    snake

--- app/src/main/java/com/example/myapplication/MyThread.java
+++ app/src/main/java/com/example/myapplication/MyThread.java
@@ -69,7 +69,7 @@ public class MyThread extends Thread {
             holder.unlockCanvasAndPost(canvas);
 
             try {
-                Thread.sleep(100);
+                Thread.sleep(10);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
